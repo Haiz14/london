0863a5c895e6da63cf9756419e97ffb3bd48a8ae
Haiz14 <haiz.gigs@gmail.com>
2024-10-22 16:34:20 +0530

    feat: installed skeleton theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,28 @@
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, aspectRatio]
+	plugins: [
+		typography, 
+		aspectRatio,
+		skeleton({
+			themes: [
+				themes.rose,
+				themes.cerberus,
+			],
+		})
+	]
 } as Config;
