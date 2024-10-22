3606294f663b94a23158a400dd5267821f00c74c
Haiz14 <haiz.gigs@gmail.com>
2024-10-22 16:35:05 +0530

    fest: updated space division

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const PAGE_TOP_MARGIN="pt-4 md:pt-8";
 export const PAGE_SIDE_MARGIN="px-2 px-4";
 export const PAGE_SECTION_SPACE="space-y-4 md:space-y-7"
-export const SAME_SECTION_SPACE="space-y-3 md:space-y-5";
+export const SAME_SECTION_SPACE="space-y-1 md:space-y-2";
